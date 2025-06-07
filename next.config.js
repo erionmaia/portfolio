d54c8cd8349634b355f1859a39ea10a84049b8d2
Erion Schlenger de Paiva Maia <erion@riseio.com>
2025-06-07 20:11:44 -0300

    fix: trying to fix to deploy.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
   images: {
-    domains: ['images.unsplash.com'],
+    unoptimized: true,
   },
 };
 
