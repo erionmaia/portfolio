23a23cc51ba3f6fba71f097ee6dba772fb2c07f4
Erion Schlenger de Paiva Maia <erion@riseio.com>
2025-06-07 20:39:15 -0300

    fix: fix next.config.js e wrangler.toml

--- next.config.js
+++ next.config.js
@@ -7,6 +7,19 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['ws', 'tls', 'stream'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        stream: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
